112f82d0f3d5ff8fcbed51180c97c1e97405a34e
asmt <asmt@itu.dk>
2021-08-25 17:05:37 +0200

    small bug fixes

--- src/util/arb_calculator.py
+++ src/util/arb_calculator.py
@@ -1,16 +1,13 @@
 def _get_arb_percentage(a, b):
-    if a == '0' or b == '0':
-        return 1000
-
     arb1 = 1 / float(a) * 100
     arb2 = 1 / float(b) * 100
     return arb1 + arb2
 
 
 def get_arb_percentage(a, b, c):
-    if a == '0' or c == '0':
+    if float(a) == 0 or float(c) == 0:
         return 1000
-    if b == '0':
+    if float(b) == 0:
         return _get_arb_percentage(a, c)
     arb1 = 1 / float(a) * 100
     arb2 = 1 / float(b) * 100
