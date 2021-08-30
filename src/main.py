a4a801cf13226a74dabb2739a94670b6b87bb6f5
asmt <asmt@itu.dk>
2021-08-30 11:17:02 +0200

    fix merging

--- src/main.py
+++ src/main.py
@@ -27,28 +27,25 @@ def run_scrapers():
 def merge(bets1, bets2):
     for time in bets2.keys():
         if time in bets1.keys():
-            for bet_1 in bets1[time]:
-                for bet_2 in bets2[time]:
+            for bet_2 in bets2[time]:
+                merged = False
+                for bet_1 in bets1[time]:
                     if ed.is_similar(bet_1.home_name, bet_2.home_name) and ed.is_similar(bet_1.away_name, bet_2.away_name):
                         bet_1.merge(bet_2)
+                        merged = True
+                        break
+                if not merged:
+                    bets1[time].append(bet_2)
         else:
             bets1[time] = bets2[time]
 
 
 start_date = datetime.date.today()
 while start_date == datetime.date.today():
-    for i in range(10):
-        #try:
-        bets = run_scrapers()
-        list = []
-        for bet_list in bets.values():
-            list.extend(bet_list)
-        break
-        #except:
-        #    time.sleep(30)
-        if i == 9:
-            winsound.Beep(440, 2000)
-            exit()
+    bets = run_scrapers()
+    list = []
+    for bet_list in bets.values():
+        list.extend(bet_list)
     list.sort()
     list.reverse()
     for bet in list:
